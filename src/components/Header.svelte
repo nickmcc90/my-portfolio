89cb6a34e248049c3d7247fc10075b723ab26680
Nicholas McCarroll <nickmccarroll8@gmail.com>
2024-07-23 20:27:02 -0400

    made a navbar a little bit of the home page

---
+++ src/components/Header.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let y;
+
+  let tabs = [
+    {name: 'Projects', link: '#projects'},
+    {name: 'About me', link: '#about'},
+  ]
+</script>
+
+<header class={"sticky top-0 z-[10] duration-200 px-6 flex items-center justify-between border border-solid " + 
+`${y > 0 ? ' py-4 bg-[#008080] border-[#3EB489]' : ' py-6 bg-transparent border-transparent'}`}>
+  <h1 class='font-medium'>
+    <b class='font-bold poppins'>Nicholas</b> McCarroll
+  </h1>
+  <div class='sm:flex items-center gap-4 hidden'>
+    {#each tabs as tab, index}
+      <a href={tab.link} class='duration-200 hover:text-[#000080]'>
+        <p>{tab.name}</p>
+      </a>
+    {/each}
+  </div>
+  <a href='https://www.linkedin.com/in/nicholas-mccarroll/'
+    target='_blank'
+    class='pinkShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white
+  text-slate-950'>
+    <div class='absolute top-0 right-full w-full h-full bg-[#fe6453] opacity-20
+    group-hover:translate-x-full z-0 duration-200 '></div>
+    <h4 class='relative z-9'>Get in touch</h4>
+  </a>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,29 @@
 <script>
   import "../app.css";
+  import Header from '../components/Header.svelte'
+  import Footer from '../components/Footer.svelte'
+
+  let y;
+  let innerHeight = 0
+  let innerWidth = 0
+
+  function goTop() {
+    document.body.scrollIntoView()
+  }
 </script>
 
-<div class='relative flex flex-col max-w-[1400px] mx-auto w-full
-text-sm sm:text-base min-h-screen'>
-
+<div class='relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen'>
+  <div class={'fixed bottom-0 w-full duration-200 flex p-10 z-[10] ' + 
+  `${y > 0 ? " opacity-full pointer-events-auto" : " pointer-events-none opacity-0"}`}>
+    <button on:click={goTop} class='ml-auto rounded-full bg-tan text-violet-400 px-3 hover:bg-slate-800
+    cursor-pointer'>
+      <i class="fa-solid fa-arrow-up grid place-items-center aspect-square"></i>
+    </button>
+  </div>
+  <Header y={y}/>
+  <slot />
+  <Footer />
 </div>
-<slot />
+
+
+<svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
